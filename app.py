1efc5eb2edc5e1816b91868e1815e1949fd8df46
3k100612-max <3k100612@gmail.com>
2026-07-24 02:16:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -347,12 +347,13 @@ def export_excel():
         buf,
         as_attachment=True,
         download_name=filename,
-        mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    )
-      except Exception as e:
+        mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+         except Exception as e:
         import traceback
         traceback.print_exc()  # this will show in your Hostinger app logs
         return jsonify({"status": "error", "message": str(e)}), 500
+    )
+     
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8506)
